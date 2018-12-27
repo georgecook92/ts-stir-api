6097e418a393f71320aaaf55251090ec6bf0f33a
George <george.cook@redant.com>
2018-12-27 21:07:38 +0000

    add: register controller

--- src/v1/controllers/authController/authController.ts
+++ src/v1/controllers/authController/authController.ts
@@ -1,10 +1,13 @@
-import { Request, Response } from "express"
-import { LoginType } from "./types"
-import { isLeft } from "fp-ts/lib/Either"
+import { Request, Response } from 'express'
+import { getCustomRepository } from 'typeorm'
+import * as joi from 'joi'
+
+import { LoginType, RegisterType, RegisterJoiTypes } from './types'
+import { isLeft } from 'fp-ts/lib/Either'
 
 import UserRepository from '../../repositories/UserRepository'
-import { getCustomRepository } from "typeorm";
-import { jwt } from '../../../util';
+import { jwt } from '../../../util'
+import { User } from '../../../models'
 
 class AuthController {
   login = async (req: Request, res: Response) => {
@@ -23,6 +26,20 @@ class AuthController {
       } else {
         res.status(401).json({ error: 'Unauthorised' })
       }
+    }
+  }
+
+  register = async (req: Request, res: Response) => {
+    const result = RegisterType.decode(req.body)
+    const joiResult = joi.validate(req.body, RegisterJoiTypes)
+
+    if (isLeft(result) || joiResult.error) {
+      res.status(400).json({ error: 'Invalid request body' })
+    } else {
+      const userRepo = getCustomRepository(UserRepository)
+      await userRepo.save(new User(result.value))
+
+      return { success: true }
     }
   }
 }
--- src/v1/controllers/authController/types.ts
+++ src/v1/controllers/authController/types.ts
@@ -1,6 +1,21 @@
 import * as t from "io-ts"
+import * as joi from 'joi'
 
 export const LoginType = t.interface({
     email: t.string,
     password: t.string
-});
+})
+
+export const RegisterType = t.interface({
+    firstName: t.string,
+    lastName: t.string,
+    email: t.string,
+    password: t.string
+})
+
+export const RegisterJoiTypes = joi.object({
+    firstName: joi.string().required(),
+    lastName: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+})
